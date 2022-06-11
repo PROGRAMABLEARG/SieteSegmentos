fcd165edae47646d31b62cc17ae671d4d8fa2c4d
PROGRAMABLE <mfleitas@programable.com.ar>
2022-06-11 17:03:24 -0300

    Update sietesegmentos.ino
    Co-Authored-By: Gallinero232 <107323088+Gallinero232@users.noreply.github.com>
    Co-Authored-By: Yo4507 <107323027+yo4507@users.noreply.github.com>
    Co-Authored-By: Alturadelamano <107323181+alturadelamano@users.noreply.github.com>
    Co-Authored-By: titigm <105997715+titigm@users.noreply.github.com>

--- sietesegmentos.ino
+++ sietesegmentos.ino
@@ -1,11 +1,11 @@
 const int a = 3;
-const int b = 3;
-const int c = 3;
-const int d = 3;
-const int e = 3;
-const int f = 3;
-const int g = 3;
-const int dp = 3;
+const int b = 4;
+const int c = 5;
+const int d = 6;
+const int e = 7;
+const int f = 8;
+const int g = 9;
+const int dp = 10;
 
 
 void setup() 
@@ -22,11 +22,49 @@ void setup()
 
 void loop()
 {
-myServo.write(100);
+cero();
 delay(1000);
-myServo.write(0);
+uno();
+delay(1000);
+dos();
+delay(1000);
+tres();
+delay(1000);
+cuatro();
+delay(1000);
+cinco();
+delay(1000);
+seis();
+delay(1000);
+siete();
+delay(1000);
+ocho();
+delay(1000);
+nueve();
 delay(1000);
 }
+
+
+void apagartodo(){
+digitalWrite(a, LOW);
+digitalWrite(b, LOW);
+digitalWrite(c, LOW);
+digitalWrite(d, LOW);
+digitalWrite(e, LOW);
+digitalWrite(f, LOW);
+digitalWrite(g, LOW);  
+digitalWrite(dp, LOW);  
+  }
+
+void cero(){
+  apagartodo();
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  }
 
 void uno(){
   apagartodo();
@@ -34,14 +72,73 @@ void uno(){
   digitalWrite(c, HIGH);
   }
 
-void apagartodo(){
-  digitalWrite(a, LOW);
-  digitalWrite(b, LOW);
-  digitalWrite(c, LOW);
-  digitalWrite(d, LOW);
-  digitalWrite(e, LOW);
-  digitalWrite(f, LOW);
-  digitalWrite(g, LOW);
-  digitalWrite(dp, LOW);
-  
+void dos(){
+  apagartodo();
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(g, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(d, HIGH);
   }
+
+void tres(){
+  apagartodo();
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(g, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  }
+
+void cuatro(){
+  apagartodo();
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+    }
+
+void cinco(){
+  apagartodo();
+  digitalWrite(a, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  }
+
+void seis(){
+  apagartodo();
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(c, HIGH);  
+  }
+
+void siete(){
+  apagartodo();
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  }
+
+void ocho(){
+  apagartodo();
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(d, HIGH);
+  digitalWrite(e, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);  
+    }
+
+void nueve(){
+  apagartodo();
+  digitalWrite(a, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(c, HIGH);
+  digitalWrite(f, HIGH);
+  digitalWrite(g, HIGH);  
+    }
